7d88196ec546c2d3c47f62a213da13fa19c026c3
Ilan Y <ilanydg@gmail.com>
2020-11-04 14:42:21 +0200

    Initialized frontend folder with ReactJS

---
+++ places-frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
+
+function App() {
+  return (
+      <Router>
+        <Switch>
+            <Route>
+              
+            </Route>
+            <Route>
+
+            </Route>
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;
