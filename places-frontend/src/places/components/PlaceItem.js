2176b41526b40ff3d17d138fdb8486c9f78748f7
Ilan Y <ilanydg@gmail.com>
2020-11-20 04:25:14 +0200

    Delete option added

--- places-frontend/src/places/components/PlaceItem.js
+++ places-frontend/src/places/components/PlaceItem.js
@@ -7,10 +7,24 @@ import Modal from '../../shared/components/UIElements/Modal';
 
 function PlaceItem(props) {
     const [showMap,setShowMap]=useState(false);
+    const [showConfirmModal, setShowConfirmModal]=useState(false);
 
     const openMapHandler =()=> setShowMap(true);
     
     const closeMapHandler =()=> setShowMap(false);
+
+    const showDeleteWarningHandler =()=>{
+        setShowConfirmModal(true);
+    };
+
+    const cancelDeleteHandler =()=>{
+        setShowConfirmModal(false);
+    };
+
+    const confirmDeleteHandler =()=>{
+        setShowConfirmModal(false);
+        console.log('DELETING...');
+    };
 
     return (
         <React.Fragment>
@@ -27,6 +41,23 @@ function PlaceItem(props) {
                   <Map center={props.coordinates} zoom={16}/>
                 </div>
             </Modal>
+
+            <Modal
+                show={showConfirmModal} 
+                header="Are you sure?" 
+                footerClass="placeee-item__modal-actions" 
+                footer={
+                <React.Fragment>
+                    <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+                    <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+                </React.Fragment>
+                }>
+                <p>
+                    Do you want to proceed and delete this place?
+                    Please note that it can't be undone thereafter.
+                </p>  
+            </Modal>
+
             <li className="place-item">
                 <Card ClassName="place-item__content">
                     <div className="place-item__image">
@@ -40,7 +71,7 @@ function PlaceItem(props) {
                     <div className="place-item__actions">
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
                         <Button to={`/places/${props.id}`}>EDIT</Button>
-                        <Button danger>DELETE</Button>
+                        <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
                     </div>
                 </Card>
             </li>
