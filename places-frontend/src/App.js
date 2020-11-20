7553c08fe00486ca8a4f5da1bfb2ab535f43a91f
Ilan Y <ilanydg@gmail.com>
2020-11-20 20:22:07 +0200

    Auth page added

--- places-frontend/src/App.js
+++ places-frontend/src/App.js
@@ -7,6 +7,7 @@ import Users from './user/pages/Users';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 
 
 function App() {
@@ -27,6 +28,9 @@ function App() {
               </Route>
               <Route path="/places/:placeId">
                 <UpdatePlace/>
+              </Route>
+              <Route path="/auth">
+                <Auth/>
               </Route>
               <Redirect to="/"/>
           </Switch>
