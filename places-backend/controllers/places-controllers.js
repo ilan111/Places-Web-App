5d4367ed6e2e09dd65a4c49a4b5e081aa34c0a07
Ilan Y <ilanydg@gmail.com>
2020-11-24 15:44:58 +0200

    Bug fix - get muiltiple places by user id (array)

--- places-backend/controllers/places-controllers.js
+++ places-backend/controllers/places-controllers.js
@@ -35,24 +35,24 @@ const getPlaceById = (req, res, next) => {
     res.json({place}); // {place} => {place: place}
 };
 
-const getPlaceByUserId = (req,res, next)=>{
+const getPlacesByUserId = (req,res, next)=>{
     const userId = req.params.uid;
     
-    const place = DUMMY_PLACES.find( p => {
+    const places = DUMMY_PLACES.filter( p => {
         return p.creator === userId;
     });
 
-    if (!place) {
+    if (!places || places.length === 0) {
         //option 2 to handle an error:
-        // const error = new Error ('Could not find a place for the provided user id.');
+        // const error = new Error ('Could not find a places for the provided user id.');
         // error.code = 404;
         // return next(error);  
         return next(
-             new HttpError('Could not find a place for the provided user id.', 404)
+             new HttpError('Could not find a places for the provided user id.', 404)
         );
        }
 
-    res.json({place}); // {place} => {place: place}
+    res.json({places}); // {place} => {place: place}
 };
 
 const createPlace = (req, res ,next)=> {
@@ -93,7 +93,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
