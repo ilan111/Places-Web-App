8674b1e2d40e47c1fdd216c14afe96c618e842eb
Ilan Y <ilanydg@gmail.com>
2020-11-25 00:15:36 +0200

    Connected to Mapbox API to return location

--- places-backend/controllers/places-controllers.js
+++ places-backend/controllers/places-controllers.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
     {
@@ -56,13 +57,21 @@ const getPlacesByUserId = (req,res, next)=>{
     res.json({places}); // {place} => {place: place}
 };
 
-const createPlace = (req, res ,next)=> {
+const createPlace = async (req, res ,next)=> {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        throw new HttpError('Invalid inputs passed, please check your data.', 422)
+        next (new HttpError('Invalid inputs passed, please check your data.', 422));
     }
 
-    const { title, description, coordinates, address, creator } = req.body;
+    const { title, description, address, creator } = req.body;
+   
+    let coordinates;
+    try{
+         coordinates = await getCoordsForAddress(address);
+    } catch(error){
+      return next(error);
+    }
+   
     //const title = req.body.title
     const createdPlace = {
         id: uuidv4(),
---
+++ places-backend/util/location.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const HttpError = require('../models/http-error');
+
+const API_KEY = 'pk.eyJ1IjoidGQxMDUiLCJhIjoiY2toOHlvcjZpMDNxbjJ1cGppanoyemEwYyJ9.nxXhwsYwG4hbcutLUPVPhg';
+
+async function getCoordsForAddress(address) {
+  const response = await axios.get(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+        address
+      )}.json?access_token=${API_KEY}`
+  );
+
+
+  const data = response.data;
+  if (!data || data.status === 'ZERO_RESULTS') {
+    const error = new HttpError(
+      'Could not find location for the specified address.',
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.features[0].center;
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
