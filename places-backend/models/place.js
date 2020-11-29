b42bec93574a3b63a7876734f974e538658edad2
Ilan Y <ilanydg@gmail.com>
2020-11-29 15:47:53 +0200

    fixes + user model added

--- places-backend/models/place.js
+++ places-backend/models/place.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
-    title: { type: String, reqired: true},
-    description: { type: String, reqired: true},
-    image: { type: String, reqired: true},
-    address: { type: String, reqired: true},
-    location: {type: Object, reqired: true},
-    creator: { type: String, reqired: true}
+    title: { type: String, required: true},
+    description: { type: String, required: true},
+    image: { type: String, required: true},
+    address: { type: String, required: true},
+    location: {type: Object, required: true},
+    creator: { type: String, required: true}
 });
 
 module.exports = mongoose.model('Place', placeSchema);
